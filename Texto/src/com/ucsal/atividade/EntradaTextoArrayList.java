877e6de6a854f5e6b8a64ee94b7dc6174eeb6c7c
JessPergentino <jessicapergentino@hotmail.com>
2018-03-09 20:27:25 -0300

    Atividade feita

---
+++ Texto/src/com/ucsal/atividade/EntradaTextoArrayList.java
@@ -0,0 +1,47 @@
+package com.ucsal.atividade;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntradaTextoArrayList {
+
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("resouser/results.csv");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String s = br.readLine(); // primeira linha
+		List<String> lista = new ArrayList<>();
+
+		int tamanho = 23778;
+
+		long inicio = System.currentTimeMillis();
+		for (int i = 0; i < tamanho; i++) {
+			lista.add(0, s);
+			s = br.readLine();
+		}
+		long fim = System.currentTimeMillis();
+		System.out.println("Array adiciona no começo: " + (fim - inicio) / 1000.0);
+		
+		inicio = System.currentTimeMillis();
+		for (int i = 0; i < tamanho; i++) {
+			lista.get(i);
+		}
+		fim = System.currentTimeMillis();
+		System.out.println("Array percorrendo: " + (fim - inicio) / 1000.0);
+
+		inicio = System.currentTimeMillis();
+		for (int i = 0; i < tamanho; i++) {
+			lista.remove(0);
+		}
+		fim = System.currentTimeMillis();
+		System.out.println("Array remove do começo: " + (fim - inicio) / 1000.0);
+		br.close();
+
+	}
+}
