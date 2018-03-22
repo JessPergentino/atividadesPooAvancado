d30c0ede9ed549e75b7bfac0cc313d083ea9e13b
JessPergentino <jessicapergentino@hotmail.com>
2018-03-22 17:43:11 -0300

    Atividade Feita - Lendo Arquivo Circuits.csv e imprimindo informações

--- AtividadeFormula1/src/br/edu/ucsal/formula/Circuito.java
+++ AtividadeFormula1/src/br/edu/ucsal/formula/Circuito.java
@@ -1,9 +1,115 @@
 package br.edu.ucsal.formula;
 
-public class Circuito {
+public class Circuito implements Comparable<Circuito>{
 
 	private int idCircuito;
 	private String refCircuito;
 	private String nomeCircuito;
+	private String local;
+	private String pais;
+	private String latitude;
+	private String longitude;
+	private String altitude;
+	private String url;
+
+	public Circuito(int idCircuito, String refCircuito, String nomeCircuito, String local, String pais, String latitude,
+			String longitude, String altitude, String url) {
+		this.idCircuito = idCircuito;
+		this.refCircuito = refCircuito;
+		this.nomeCircuito = nomeCircuito;
+		this.local = local;
+		this.pais = pais;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.altitude = altitude;
+		this.url = url;
+	}
+
+	public int getIdCircuito() {
+		return idCircuito;
+	}
+
+	public void setIdCircuito(int idCircuito) {
+		this.idCircuito = idCircuito;
+	}
+
+	public String getRefCircuito() {
+		return refCircuito;
+	}
+
+	public void setRefCircuito(String refCircuito) {
+		this.refCircuito = refCircuito;
+	}
+
+	public String getNomeCircuito() {
+		return nomeCircuito;
+	}
+
+	public void setNomeCircuito(String nomeCircuito) {
+		this.nomeCircuito = nomeCircuito;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getAltitude() {
+		return altitude;
+	}
+
+	public void setAltitude(String altitude) {
+		this.altitude = altitude;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+
+		return "Circuito id= " + this.idCircuito + " Referencia: " + this.refCircuito + " Nome: " + this.nomeCircuito
+				+ " Local: " + this.local + " Pais: " + this.pais + " Latitude: " + this.latitude + " Longitude: "
+				+ this.longitude + " Altitude: " + this.altitude + " URL: " + this.url;
+	}
+
+	@Override
+	public int compareTo(Circuito c) {
+		
+		return this.getRefCircuito().compareTo(c.getRefCircuito());
+	}
+	
 	
 }
--- AtividadeFormula1/src/br/edu/ucsal/formula/LerArquivo.java
+++ AtividadeFormula1/src/br/edu/ucsal/formula/LerArquivo.java
@@ -1,5 +1,69 @@
 package br.edu.ucsal.formula;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class LerArquivo {
 
+	public static void main(String[] args) {
+		List<Circuito> circuitos = new ArrayList<Circuito>();
+
+		File arquivo = new File("circuits.csv");
+
+		try {
+			FileInputStream fileIntuptStream = new FileInputStream(arquivo);
+			InputStreamReader inputStreamReader = new InputStreamReader(fileIntuptStream);
+			BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+			String line = "";
+			bufferReader.readLine();
+			while ((line = bufferReader.readLine()) != null) {
+				String[] array = line.split(",");
+
+				Circuito circuito = new Circuito(Integer.parseInt(array[0]), array[1], array[2], array[3], array[4],
+						array[5], array[6], array[7], array[8]);
+				circuitos.add(circuito);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		System.out.println(circuitos.size());
+
+		for (Circuito circuito : circuitos) {
+			System.out.println(circuito);
+		}
+
+		System.out.println();
+		
+		Collections.sort(circuitos, Collections.reverseOrder());
+
+		for (Circuito circuito : circuitos) {
+			System.out.println(circuito);
+		}
+		
+		System.out.println();
+
+		Compare c = new Compare();
+		Collections.sort(circuitos, c);
+
+		for (Circuito circuito : circuitos) {
+			System.out.println(circuito);
+		}
+
+	}
+
+}
+
+class Compare implements Comparator<Circuito> {
+	@Override
+	public int compare(Circuito o1, Circuito o2) {
+
+		return o1.getRefCircuito().compareTo(o2.getRefCircuito()) * -1;
+	}
 }
