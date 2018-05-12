84348d70793e1cd1073263c67f80dd550b90d19f
100912556 <100912556@PT1L303.ucsal.br>
2018-05-12 12:08:24 -0300

    atualização

--- Thread/src/br/com/atividade/pilha/Teste.java
+++ Thread/src/br/com/atividade/pilha/Teste.java
@@ -6,22 +6,25 @@ public class Teste {
 		Pilha pilha = new Pilha();
 		
 		Produtor produtor = new Produtor(pilha);
-		Thread thread2= new Thread(produtor);
-		thread2.start();
+		Produtor produtor2 = new Produtor(pilha);
+		Produtor produtor3 = new Produtor(pilha);
+		
+		Thread thread2 = new Thread(produtor);
+		Thread thread3 = new Thread(produtor2);
+		Thread thread5 = new Thread(produtor3);
 		
 		Consumidor consumidor = new Consumidor(pilha);
-		Thread thread1 = new Thread(consumidor);
-		thread1.start();
-		
-		Produtor produtor2 = new Produtor(pilha);
-		Thread thread3= new Thread(produtor2);
-		thread3.start();
-		
 		Consumidor consumidor2 = new Consumidor(pilha);
+		
 		Thread thread4 = new Thread(consumidor2);
+		Thread thread1 = new Thread(consumidor);
+		
+		
+		
+		thread1.start();
 		thread4.start();
 		
-		
+	
 		
 		
 		
